aeb0cf65e81da79fa25ca8e0d54e936e0d35be20
Hai Ming <34784358+hmn24@users.noreply.github.com>
2019-07-26 21:58:20 +0800

    Update readPID.cpp

--- lib/readPID.cpp
+++ lib/readPID.cpp
@@ -6,19 +6,18 @@
 extern "C"
 
 // Read PI IO function
-K readPidFile(K pid, K file, K kLines)
+K readPidFile(K pid, K file)
 {
     K k,v; 
-    J i, lines, ival;
+    J i, ival;
     std::string line; 
 
-    if((file->t!=-KS) || (pid->t!=-KJ) || (kLines->t!=-KJ)) {return krr((S) "type");};
-    lines = kLines -> j;
+    if((file->t!=-KS) || (pid->t!=-KI)) {return krr((S) "type");};
     k = ktn(KS, 0); 
     v = ktn(KJ, 0);
-    std::ifstream reader("/proc/" + std::to_string(pid -> j) + "/" + std::string(file -> s));
+    std::ifstream reader("/proc/" + std::to_string(pid -> i) + "/" + std::string(file -> s));
     if(!reader) {return krr((S) "Error opening file for output");}
-    for (i=0; (i < lines) && !reader.eof(); i++)
+    for (i=0; !reader.eof(); i++)
     {
         getline(reader,line,':');
         if(line.length()){js(&k, ss((S) line.c_str()));} 
@@ -29,3 +28,5 @@ K readPidFile(K pid, K file, K kLines)
 
     return xD(k,v);
 };  
+
+
