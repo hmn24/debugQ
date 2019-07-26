7a55c88f85fc7e8f399408cb72b0483ac85258f4
Hai Ming <34784358+hmn24@users.noreply.github.com>
2019-07-26 21:48:24 +0800

    Update readPID.cpp

--- lib/readPID.cpp
+++ lib/readPID.cpp
@@ -9,24 +9,23 @@ extern "C"
 K readPidFile(K pid, K file, K kLines)
 {
     K k,v; 
-    J i, lines;
+    J i, lines, ival;
     std::string line; 
-    try 
+
+    if((file->t!=-KS) || (pid->t!=-KJ) || (kLines->t!=-KJ)) {return krr((S) "type");};
+    lines = kLines -> j;
+    k = ktn(KS, 0); 
+    v = ktn(KJ, 0);
+    std::ifstream reader("/proc/" + std::to_string(pid -> j) + "/" + std::string(file -> s));
+    if(!reader) {return krr((S) "Error opening file for output");}
+    for (i=0; (i < lines) && !reader.eof(); i++)
     {
-        if((file->t!=-KS) || (pid->t!=-KJ) || (kLines->t!=-KJ)) {krr((S) "type");};
-        lines = kLines -> j;
-        k = ktn(KS, lines); 
-        v = ktn(KJ, lines);
-        std::ifstream reader("/proc/" + std::to_string(pid -> j) + "/" + std::string(file -> s));
-        for (i=0; i < lines; i++)
-        {
-        getline(reader,line,':'); kS(k)[i] = ss((S) line.c_str());
-        getline(reader,line,'\n'); kJ(v)[i] = (J) std::stoi(line);
-        };
-        reader.close();
-    }
-    catch (std::exception &e) {krr((S) e.what());}
+        getline(reader,line,':');
+        if(line.length()){js(&k, ss((S) line.c_str()));} 
+        getline(reader,line,'\n'); 
+        if(line.length()){ival = std::stoi(line); ja(&v, &ival);}
+    };
+    reader.close();
+
     return xD(k,v);
 };  
-
-
