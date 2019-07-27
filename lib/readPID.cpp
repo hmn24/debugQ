451e5a323c9f652b49848f016f6618c94fa26f90
Hai Ming <34784358+hmn24@users.noreply.github.com>
2019-07-27 19:06:49 +0800

    Update readPID.cpp

--- lib/readPID.cpp
+++ lib/readPID.cpp
@@ -32,24 +32,33 @@ K readPidFile(K pid, K file)
 
 extern "C" 
 
-// Temporary implementation of /proc/pid/stats, which returns a list of strings  
-K readPidStat(K pid, K sI, K eI)
-{
-    J length, i; 
-    std::string line; // Standard definition of variables
-    
-    if((pid->t!=-KI) || (sI->t!=-KJ) || (eI->t!=-KJ)) {return krr((S) "type");}; 
-    if(eI->j >= sI->j) {length = 1 + eI->j - sI->j;} else {return krr((S) "Ending Index must be larger than Starting Index");}; 
-    K v = ktn(0, length);
+extern "C" 
 
+// Temporary implementation of /proc/pid/stats, which returns a list of strings
+// This assumes strings come in ascending format which would be handle on the q level
+K readPidStat(K pid, K list)
+{
+    J i, ind = 0; 
+    std::string line; // Standard definition of variables
+
+    if((pid->t!=-KI) || (list->t!=KJ)) {return krr((S) "type");}; 
+    if(0 == list->n) {return krr((S) "Must provide at least one number");}; 
+    K v = ktn(0, list->n);
+
+    J eI = kJ(list)[list->n - 1];
     std::ifstream reader("/proc/" + std::to_string(pid -> i) + "/stat");
     if(!reader) {return krr((S) "Error opening file for output");}
-    for (i=1; (!reader.eof()); i++)
+    for (i=1; !reader.eof(); i++)
     {
     getline(reader,line,' ');
-    if(i >= sI->j) {kK(v)[i - sI->j] = kp((S) line.c_str()); if(i == eI->j){break;};}
+    if(i <= eI) {if(i==kJ(list)[ind]){kK(v)[ind] = kp((S) line.c_str()); ind++;};} else {break;}
     }
+    if(0 <= (list->n)-ind-1){for(ind;ind<list->n;ind++){kK(v)[ind] = kp((S)" ");};}; // Create null values for empty ind
     reader.close();
+
     return v;
 };
+
+// Not necessary here but to create an exception in C++
+// try {} catch (std::exception &e) {krr((S) e.what());};
 
